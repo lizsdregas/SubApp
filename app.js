83f5c46c2d2c71abca00bbfd39ea234d3c555ff5
lizsdregas <liz@sdregas.com>
2016-06-15 17:12:50 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+﻿
+//////// angularfire
+
+var app = angular.module("subApp", ["firebase"]);
+
+app.controller("SubController", ["$scope", "$firebaseObject", controller]);
+
+function controller($scope, $firebaseObject) { 
+
+    // download the data into a local object 
+    var ref = new Firebase("https://sub-spa.firebaseio.com");
+    $scope.data = $firebaseObject(ref);
+    
+    $scope.requests = $firebaseObject(ref.child('requests'));
+   
+    //remove request 
+    $scope.removeRequest = function (id) {
+        var testRef = new Firebase("https://sub-spa.firebaseio.com");
+        var newRef = testRef.child(id);
+        newRef.$remove(); 
+    }  
+
+
+}
